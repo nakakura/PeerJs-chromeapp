7f5b51915d73874012e63387098215e25cc9a362
Toshiya Nakakura <t.nakakura@ntt.com>
2013-12-03 20:42:37 +0900

    start button

--- packaged-apps/foreground/foreground.js
+++ packaged-apps/foreground/foreground.js
@@ -1,3 +1,13 @@
-///<reference path="./peerjs_server.ts"/>
-var peer = PeerJsServer.getInstance(new PeerJsOptions("peerjs", 9000));
+$(function () {
+    console.log("hogehoge");
+    $("#start").click(function(){
+        var key = $("#key")[0].value;
+        var port = parseInt($("#port")[0].value, 10);
+        var peer = PeerJsServer.getInstance(new PeerJsOptions(key, port));
+        $("#start").hide();
+    });
+
+});
+
+
 //# sourceMappingURL=foreground.js.map
